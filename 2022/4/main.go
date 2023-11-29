971200b994707e537ffcf54c6fcb1089f2154ef2
Stephen Condon <scondon87@gmail.com>
2023-11-28 20:36:30 -0600

    Resolved Part 2

--- 2022/4/main.go
+++ 2022/4/main.go
@@ -16,50 +16,42 @@ func main() {
 
 func solveChallenge(filename string) int {
 	data := readInput(filename)
-	numFullyContained := 0
+	numOverlap := 0
 	for _, line := range data {
 		elves := strings.Split(line, ",")
 		processedElves := map[int][]int{
 			0: nil,
 			1: nil,
 		}
-		// ["start1-end1", "start2-end2"]
+
 		for index, assignment := range elves {
 			elfAssignment := strings.Split(assignment, "-")
 			firstElfAssignment, _ := strconv.Atoi(elfAssignment[0])
 			secondElfAssignment, _ := strconv.Atoi(elfAssignment[1])
 			processedElves[index] = append(processedElves[index], firstElfAssignment)
 			processedElves[index] = append(processedElves[index], secondElfAssignment)
-			//[start, end]
 		}
 
-		if hasFullyContainedPair(processedElves) {
-			numFullyContained = numFullyContained + 1
+		if doAssignmentsOverlap(processedElves) {
+			numOverlap = numOverlap + 1
 		}
 	}
-	// split on , to get the two ranges
-	// split each range on - to get the two sets of start/end
-	// evaluate if one range is fully contained in another
-	return numFullyContained
+	return numOverlap
 }
 
-func hasFullyContainedPair(assignments map[int][]int) bool {
+func doAssignmentsOverlap(assignments map[int][]int) bool {
 	result := false
-	if assignments[0][0] > assignments[1][0] {
-		// second elf is candidate
-		if assignments[0][1] <= assignments[1][1] {
+
+	if assignments[0][0] < assignments[1][0] {
+		if assignments[0][1] >= assignments[1][0] {
 			result = true
 		}
-	} else if assignments[0][0] < assignments[1][0] {
-		// first elf is candidate
-		if assignments[0][1] >= assignments[1][1] {
-			// true
+	} else if assignments[0][0] > assignments[1][0] {
+		if assignments[1][1] >= assignments[0][0] {
 			result = true
 		}
 	} else {
 		result = true
-		// start at same point
-		// true
 	}
 
 	return result
