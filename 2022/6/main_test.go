c625c9c93e17e1642ce73656c7907c22d4c702e9
Stephen Condon <scondon87@gmail.com>
2023-11-30 13:49:54 -0600

    Solved part 1

---
+++ 2022/6/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func Test_Sample(t *testing.T) {
+	type test struct {
+		filename string
+		want     int
+	}
+
+	tests := []test{
+		{filename: "sample5.txt", want: 5},
+		{filename: "sample6.txt", want: 6},
+		{filename: "sample7.txt", want: 7},
+		{filename: "sample10.txt", want: 10},
+		{filename: "sample11.txt", want: 11},
+		{filename: "input.txt", want: 1566},
+	}
+
+	for _, tc := range tests {
+		got := solveChallenge(tc.filename)
+		if got != tc.want {
+			t.Fatalf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+}
