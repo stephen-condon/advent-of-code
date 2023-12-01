43b8c558f723620b4f1bd01f6c4dc62c08dff2d0
Stephen Condon <scondon87@gmail.com>
2023-12-01 10:31:34 -0600

    Solve part 1

---
+++ 2023/1/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	result := solvePartOne("input.txt")
+
+	log.Printf(`Result: %v`, result)
+}
+
+func solvePartOne(filename string) int {
+	sum := 0
+	data := readInput(filename)
+
+	r, _ := regexp.Compile(`\d`)
+
+	for _, line := range data {
+		splitLine := strings.Split(line, "")
+		matches := r.FindAllStringIndex(line, -1)
+
+		firstDigit, _ := strconv.Atoi(splitLine[matches[0][0]])
+		secondDigit, _ := strconv.Atoi(splitLine[matches[len(matches)-1][0]])
+
+		calibrationValue := (firstDigit * 10) + secondDigit
+
+		sum = sum + calibrationValue
+	}
+
+	return sum
+}
+
+func readInput(filename string) []string {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// remember to close the file at the end of the program
+	defer f.Close()
+
+	// read the file line by line using scanner
+	scanner := bufio.NewScanner(f)
+	var dataSlice []string
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		dataSlice = append(dataSlice, text)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return dataSlice
+}
