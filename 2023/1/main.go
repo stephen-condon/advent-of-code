164722e2d9956bc40a4a6bc52c5a5b35fd1e3a2a
Stephen Condon <scondon87@gmail.com>
2023-12-01 12:20:04 -0600

    Solve part 2
    Added additional example of scenario where my initial approach didn't handle correctly

--- 2023/1/main.go
+++ 2023/1/main.go
@@ -5,26 +5,66 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"sort"
 	"strconv"
 )
 
+var numberTextLookup map[string]int
+var regexString []string
+
 func main() {
-	result := solvePartOne("input.txt")
+	result := solvePartTwo("input.txt")
 
 	log.Printf(`Result: %v`, result)
 }
 
 func solvePartOne(filename string) int {
-	sum := 0
+	regexString = []string{`\d`}
+
 	data := readInput(filename)
 
-	r, _ := regexp.Compile(`\d`)
+	return calculateSumCalibrationValues(data)
+}
+
+func solvePartTwo(filename string) int {
+	initNumberTextLookup()
+	regexString = []string{`\d`, `(?:one)`, `(?:two)`, `(?:three)`, `(?:four)`, `(?:five)`, `(?:six)`, `(?:seven)`, `(?:eight)`, `(?:nine)`}
+
+	data := readInput(filename)
+
+	return calculateSumCalibrationValues(data)
+}
+
+func calculateSumCalibrationValues(data []string) int {
+	sum := 0
 
 	for _, line := range data {
-		matches := r.FindAllStringIndex(line, -1)
+		matches := [][]int{}
+		// need to iterate through regex, and dynamically build matches, based on example in bug.txt
+		// there may be a better regex way to handle this, but I haven't figured it out
+		for _, value := range regexString {
+			r, _ := regexp.Compile(value)
+			individualMatches := r.FindAllStringIndex(line, -1)
+			matches = append(matches, individualMatches...)
+		}
+		sort.SliceStable(matches, func(i, j int) bool { return matches[i][0] < matches[j][0] })
 
-		firstDigit, _ := strconv.Atoi(line[matches[0][0]:matches[0][1]])
-		secondDigit, _ := strconv.Atoi(line[matches[len(matches)-1][0]:matches[len(matches)-1][1]])
+		firstMatch := line[matches[0][0]:matches[0][1]]
+		lastMatch := line[matches[len(matches)-1][0]:matches[len(matches)-1][1]]
+		var firstDigit, secondDigit int
+
+		if len(firstMatch) > 1 {
+			// process string number
+			firstDigit = translateNumberText(firstMatch)
+		} else {
+			firstDigit, _ = strconv.Atoi(line[matches[0][0]:matches[0][1]])
+		}
+		if len(lastMatch) > 1 {
+			// process string number
+			secondDigit = translateNumberText(lastMatch)
+		} else {
+			secondDigit, _ = strconv.Atoi(line[matches[len(matches)-1][0]:matches[len(matches)-1][1]])
+		}
 
 		calibrationValue := (firstDigit * 10) + secondDigit
 
@@ -32,6 +72,25 @@ func solvePartOne(filename string) int {
 	}
 
 	return sum
+}
+
+func initNumberTextLookup() {
+	numberTextLookup = map[string]int{
+		"zero":  0,
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+}
+
+func translateNumberText(text string) int {
+	return numberTextLookup[text]
 }
 
 func readInput(filename string) []string {
--- 2023/1/main_test.go
+++ 2023/1/main_test.go
@@ -20,3 +20,23 @@ func Test_PartOne(t *testing.T) {
 		}
 	}
 }
+
+func Test_PartTwo(t *testing.T) {
+	type test struct {
+		filename string
+		want     int
+	}
+
+	tests := []test{
+		{filename: "sample2.txt", want: 281},
+		{filename: "bug.txt", want: 12},
+		{filename: "input.txt", want: 54649},
+	}
+
+	for _, tc := range tests {
+		got := solvePartTwo(tc.filename)
+		if got != tc.want {
+			t.Fatalf("expected: %v, got: %v", tc.want, got)
+		}
+	}
+}
