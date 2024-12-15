96eae1b4effa830b93c24af33f64ea8e489da8e7
Stephen Condon <scondon87@gmail.com>
2024-12-14 20:11:50 -0600

    initialized module, added shared function

---
+++ 2024/go.mod
@@ -0,0 +1,3 @@
+module scondon87/advent-ofcode/2024
+
+go 1.22.0
---
+++ 2024/readinput/readinput.go
@@ -0,0 +1,31 @@
+package readinput
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func Read(filename string) []string {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// remember to close the file at the end of the program
+	defer f.Close()
+
+	// read the file line by line using scanner
+	scanner := bufio.NewScanner(f)
+	var dataSlice []string
+
+	for scanner.Scan() {
+		text := scanner.Text()
+		dataSlice = append(dataSlice, text)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	return dataSlice
+}
