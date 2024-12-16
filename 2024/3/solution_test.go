29e6047e16a1b6606193fa30d3738d0a61d25c35
Stephen Condon <scondon87@gmail.com>
2024-12-15 22:06:59 -0600

    Solved part 2

--- 2024/3/solution_test.go
+++ 2024/3/solution_test.go
@@ -19,3 +19,30 @@ func TestSolutionInput(t *testing.T) {
 	}
 
 }
+
+func TestSolution2Example(t *testing.T) {
+	result := solution2("./example2.txt")
+
+	if result != 48 {
+		t.Fatalf(`Expected 48, received %v`, result)
+	}
+
+}
+
+func TestSolution2Example3(t *testing.T) {
+	result := solution2("./example3.txt")
+
+	if result != 2791426 {
+		t.Fatalf(`Expected 2791426, received %v`, result)
+	}
+
+}
+
+func TestSolution2Input(t *testing.T) {
+	result := solution2("./input.txt")
+
+	if result != 74361272 {
+		t.Fatalf(`Expected 74361272, received %v`, result)
+	}
+
+}
